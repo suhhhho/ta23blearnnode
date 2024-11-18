32f5fa423875f9b55ef0a9953310193367d7b69d
Sofja <sofja.portnova@tptlive.ee>
2024-11-18 11:50:38 +0200

    add character

--- learnnode/webpack.config.js
+++ learnnode/webpack.config.js
@@ -1,21 +1,43 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
+ 
 const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
+ 
+let name = "Sofja Portnova"
+ 
+ 
+export default async() =>{
+    const response = await fetch("https://rickandmortyapi.com/api/character")
+const data = await response.json()
+const characters = data.results
+const pages = [];
+characters.forEach(character => {
+  let page = new HtmlWebpackPlugin({
+      template: './src/about.njk',
+      filename:`character_${character.id}.html`,
+      templateParameters:{
+        character,
+      }
 
-export default {
+  });
+  pages.push(page)
+});
+console.log(characters)
+return {
+
+
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path:path.resolve(__dirname,'dist')
+        path: path.resolve(__dirname, 'dist')
     },
     devServer: {
         static: {
             directory: path.join(__dirname, 'public'),
         },
         compress: true,
-        port:9000,
+        port: 9000,
     },
     module: {
         rules: [
@@ -24,15 +46,15 @@ export default {
                 use: ["style-loader", "css-loader"],
             },
             {
-                test: /\.scss$/i,
+                test: /\.s[ac]ss$/i,
                 use: [
-                    "style-loader", 
-                    "css-loader" ,
+                    "style-loader",
+                    "css-loader",
                     {
-                        loader:"sass-loader",
-                        options:{
+                        loader: 'sass-loader',
+                        options: {
                             sassOptions: {
-                                quietDeps:true,
+                                quietDeps: true,
                             }
                         }
                     }
@@ -52,12 +74,15 @@ export default {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.njk',
+            templateParameters: {
+               name, characters
+            }
         }),
         new HtmlWebpackPlugin({
             template: './src/about.njk',
             filename: 'about.html'
         }),
-
+        ...pages
     ],
-
+}
 }
