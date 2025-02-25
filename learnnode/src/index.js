9769e6cca63fdf9383cef04a036fab406e371880
Sofja <sofja.portnova@tptlive.ee>
2025-02-25 13:59:55 +0200

    commit

--- learnnode/src/index.js
+++ learnnode/src/index.js
@@ -8,11 +8,13 @@ import App from './App.vue';
 import ToDo from './pages/ToDo.vue';
 import Modals from './pages/Modals.vue';
 import ChuckNorris from './pages/ChuckNorris.vue';
+import RickAndMorty from './pages/RickAndMorty.vue';
 
 const routes = [
   { path: '/', component: ToDo, name: 'ToDo' },
   { path: '/modals', component: Modals, name: 'Modals' },
   { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris' },
+  { path: '/rickandmorty', component: RickAndMorty, name: 'Rick And Morty' },
 ];
 
 const router = createRouter({
---
+++ learnnode/src/pages/RickAndMorty.vue
@@ -0,0 +1,30 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+let response = await axios.get('https://rickandmortyapi.com/api/character');
+console.log(response.data);
+let characters = ref(response.data.results);
+let pagination = ref(response.data.info);
+</script>
+
+<template>
+    <div class="columns">
+        <div class="column" v-for="character in characters">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">Card header</p>
+                </header>
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="Placeholder image" />
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="content">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
