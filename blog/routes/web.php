a7beaad4cedf6dd75697d1621feb3fad6623a746
Sofja <sofja.portnova@tptlive.ee>
2025-12-14 15:35:15 +0200

    Add laravel breeze

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,10 +12,22 @@ Route::get('/post/{post}', [PublicController::class, 'post'])->name('post');
 // Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
 // Route::post('/admin/posts', [PostController::class, 'store'])->name('posts.store');
 // Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-// Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+//     Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 // Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 Route::get('/admin/posts/deleted', [PostController::class, 'deleted'])->name('posts.deleted');
 Route::post('/admin/posts/{post}/restore', [PostController::class, 'restore'])->name('posts.restore');
 Route::delete('/admin/posts/{post}/permadestroy', [PostController::class, 'permaDestroy'])->name('posts.permadestroy');
 Route::resource('/admin/posts', PostController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
